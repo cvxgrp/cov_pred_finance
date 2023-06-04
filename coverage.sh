cffcf21bcd8374f3698f1c4e57e5f294d752106e
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-04 16:36:02 -0700

    coverage script

---
+++ coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+poetry run coverage run --source=cvx/. -m pytest
+poetry run coverage report -m
+poetry run coverage html
+if [[ $OSTYPE == 'darwin'* ]]; then
+    open htmlcov/index.html
+fi
+if [[ $OSTYPE == 'linux'* ]]; then
+    xdg-open htmlcov/index.html
+fi
